00df5f4558ce46c1df6521c047acaae81e4ecf1e
Dorina Ferencsik <dorina.ferencsik@gmail.com>
2020-08-30 14:34:17 +0200

    Feat: Endpoint calls and basic interfaces

---
+++ src/app/posts/services/api-post.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CustomHttpClient } from 'src/app/shared/services/custom-http-client.service';
+
+import { IComment } from '../interfaces/comment.interface';
+import { IPost } from '../interfaces/post.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiPostService {
+
+  constructor(private httpClient: CustomHttpClient) {}
+
+  /**
+   * List posts
+   */
+  public getPostList(): Observable<IPost[]> {
+    return this.httpClient.get('posts');
+  }
+
+  /**
+   * Creat new post
+   * @param details details of new post
+   */
+  public postNewPost(details: IPost): Observable<any> {
+    return this.httpClient.post('posts', details);
+  }
+
+  /**
+   * List comments for post
+   * @param postId id of post
+   */
+  public getCommentList(postId: number): Observable<IComment[]> {
+    return this.httpClient.get(`posts/${postId}/comments`);
+  }
+}
